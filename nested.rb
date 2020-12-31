e8f106e31c87c14116d08f211611e96656a90fcc
Johndhee Ballesteros <johndheeballesteros13@gmail.com>
2020-12-31 19:35:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 # }
 # return the entire updated hash
 programmer_hash[:yukihiro_matsumoto] = 
-[{:known_for => "Ruby", :languages => "LISP", "C"
+[{:known_for => "Ruby", :languages => [ "LISP", "C"]}]
 	programmer_hash = 
  		{
       :grace_hopper => {
