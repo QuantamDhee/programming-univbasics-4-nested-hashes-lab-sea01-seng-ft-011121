45b399bd9ff239df8cced84c9dc194f3881ca8fe
Johndhee Ballesteros <johndheeballesteros13@gmail.com>
2020-12-31 19:45:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,7 @@ def changing_alan
       }
     }
 programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -129,5 +130,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
